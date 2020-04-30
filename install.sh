a3c0d632ffb5dfe920f1f4a331d74e106698bfd1
Timo Osterkamp <tosterkamp@uos.de>
2020-04-30 18:11:26 +0200

    add rm vimrc

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools
 sed -i 's/robbyrussell/agnoster/g' ~/.zshrc
 ln -s ~/config-files/zshrc_extensions ~/.zshrc_extensions
 echo "source ~/.zshrc_extensions" > ~/.zshrc
+rm ~/.vimrc
 ln -s ~/config-files/vimrc_extensions ~/.vimrc
 echo "set mouse-=a" > ~/.vimrc
 zsh
