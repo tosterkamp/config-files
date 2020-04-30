158910eda2f7d4994896a35c3fe33b2765109bf9
Timo Osterkamp <tosterkamp@uos.de>
2020-04-30 18:02:18 +0200

    removed newlines in install script

--- install.sh
+++ install.sh
@@ -1,16 +1,7 @@
-
 git clone https://github.com/tosterkamp/config-files.git ~/.config-repo
-
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 sed -i 's/robbyrussell/agnoster/g' ~/.zshrc
-
 ln -s ~/config-files/zshrc_extensions ~/.zshrc_extensions
-
 echo "source ~/.zshrc_extensions" > ~/.zshrc
-
 ln -s ~/config-files/vimrc_extensions ~/.vimrc
-
 echo "set mouse-=a" > ~/.vimrc
-
-
