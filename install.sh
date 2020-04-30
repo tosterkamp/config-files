e639cc6521bb8870ef10752d3430df9ffdd0c8f9
Timo Osterkamp <tosterkamp@uos.de>
2020-04-30 19:37:19 +0200

    do stuff

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 git clone https://github.com/tosterkamp/config-files.git ~/.config-repo
+
+rm ~/.vimrc
+ln -s ~/config-files/vimrc ~/.vimrc
+
+ln -s ~/config-files/zshrc_extensions ~/.zshrc_extensions
+
 alias exec="echo"
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sed -i 's/robbyrussell/agnoster/g' ~/.zshrc
-ln -s ~/config-files/zshrc_extensions ~/.zshrc_extensions
-echo "source ~/.zshrc_extensions" > ~/.zshrc
-rm ~/.vimrc
-ln -s ~/config-files/vimrc_extensions ~/.vimrc
-echo "set mouse-=a" > ~/.vimrc
 zsh
