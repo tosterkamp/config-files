6db1e73def1cbbeb20ead908bd26adbe30f8818f
Timo Osterkamp <tosterkamp@uos.de>
2020-04-30 18:08:29 +0200

    set exec to non

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 git clone https://github.com/tosterkamp/config-files.git ~/.config-repo
+alias exec="echo"
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sed -i 's/robbyrussell/agnoster/g' ~/.zshrc
 ln -s ~/config-files/zshrc_extensions ~/.zshrc_extensions
 echo "source ~/.zshrc_extensions" > ~/.zshrc
 ln -s ~/config-files/vimrc_extensions ~/.vimrc
 echo "set mouse-=a" > ~/.vimrc
+zsh
